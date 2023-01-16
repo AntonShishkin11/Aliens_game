e5854ae0ddd6cca6da5f59ed761a07cad9dcd1bc
AntonShishk <annnt97na@yandex.ru>
2023-01-16 14:44:58 +0300

    Started project.

---
+++ alien_invasion.py
@@ -0,0 +1,29 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры."""
+
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            #Отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #Отображение последнего прорисованного экрана.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #Создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
