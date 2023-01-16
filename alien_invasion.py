13c002b11e07e5a1a971166ed6863fea47dbe231
AntonShishk <annnt97na@yandex.ru>
2023-01-16 15:48:51 +0300

    Created Settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
 import sys
-
+from settings import Settings
 import pygame
 
 class AlienInvasion:
@@ -8,10 +8,11 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((600, 400))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (40, 70 ,70)
+        #Назначение цвета фона.
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -20,7 +21,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            #При каждом проходе цикла перерисовыввается экран.
+            self.screen.fill(self.settings.bg_color)
             #Отображение последнего прорисованного экрана.
             pygame.display.flip()
 
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    def __init__(self):
+        """Инициализирует настройки игры"""
+        #Параметры экрана.
+        self.screen_width = 600
+        self.screen_height = 400
+        self.bg_color = (40, 70, 70)
